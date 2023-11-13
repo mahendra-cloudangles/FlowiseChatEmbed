e383c3bf4048cebc872f47123e4340ef3674fe6b
Mahendra Singh Bora | CloudAngles <mahendra.singhbora@cloudangles.com>
2023-11-13 19:17:02 +0530

    Changed svg color

--- src/components/bubbles/LoadingBubble.tsx
+++ src/components/bubbles/LoadingBubble.tsx
@@ -1,8 +1,9 @@
-import { TypingBubble } from '@/components'
+import { TypingBubble } from "@/components";
 
 export const LoadingBubble = () => (
   <div
     class="flex justify-start mb-2 items-start animate-fade-in host-container"
+    style={{ "background-color": "#0D1322" }}
   >
     <span
       class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble"
@@ -11,4 +12,4 @@ export const LoadingBubble = () => (
       <TypingBubble />
     </span>
   </div>
-)
+);
