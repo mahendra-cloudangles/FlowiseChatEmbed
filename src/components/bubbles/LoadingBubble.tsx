a4a4fce6cd6bf470d731b02aebde63c9b275a839
Mahendra Singh Bora | CloudAngles <mahendra.singhbora@cloudangles.com>
2023-11-13 19:20:20 +0530

    Changes span color

--- src/components/bubbles/LoadingBubble.tsx
+++ src/components/bubbles/LoadingBubble.tsx
@@ -1,13 +1,11 @@
 import { TypingBubble } from "@/components";
 
 export const LoadingBubble = () => (
-  <div
-    class="flex justify-start mb-2 items-start animate-fade-in host-container"
-    style={{ "background-color": "#0D1322" }}
-  >
+  <div class="flex justify-start mb-2 items-start animate-fade-in host-container">
     <span
       class="px-4 py-4 ml-2 whitespace-pre-wrap max-w-full chatbot-host-bubble"
       data-testid="host-bubble"
+      style={{ "background-color": "#0D1322" }}
     >
       <TypingBubble />
     </span>
